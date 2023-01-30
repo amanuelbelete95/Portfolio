e2eba37f090aef84c2aa1690d79e3d35ad50f408
Amanuel Belete <amanuelbelete95@gmail.com>
2023-01-30 18:23:24 +0300

    Fix linter error

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const humburger = document.querySelector(".humburger");
+const humburger = document.querySelector('.humburger');
+
 const closeBtn = document.querySelector('.close-btn');
+
 const menu = document.querySelector('.menu-bar');
+
 const menuItems = document.querySelectorAll('.menu-mobile a');
 
-humburger.addEventListener('click', function(){
+humburger.addEventListener('click', () => {
   menu.classList.toggle('hidden-menu');
-})
-
-closeBtn.addEventListener('click', function(){
-  menu.classList.toggle('hidden-menu');
-})
-
-
-menuItems.forEach(item => {
-  item.addEventListener('click', menuRemover)
 });
 
-function menuRemover(){
+closeBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden-menu');
+});
+
+function menuRemover() {
   menu.classList.toggle('hidden-menu');
 }
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', menuRemover);
+});
