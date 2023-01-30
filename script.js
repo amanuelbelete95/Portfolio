a91fecf02ba75845350a221b867e768242ae0824
Amanuel Belete <amanuelbelete95@gmail.com>
2023-01-30 21:21:14 +0300

    Modify linters.yml file

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const humburger = document.querySelector('.humburger');
-
 const closeBtn = document.querySelector('.close-btn');
-
 const menu = document.querySelector('.menu-bar');
-
 const menuItems = document.querySelectorAll('.menu-mobile a');
 
 humburger.addEventListener('click', () => {
